e72d1c2ebe48c282882434b99ea112b18284f57c
Andrii Volin <angy.volin@gmail.com>
2017-04-30 01:11:47 +0300

    add module suppot

--- jquery.remoteclock.js
+++ jquery.remoteclock.js
@@ -1,6 +1,23 @@
-+function ($) {
-    'use strict';
+/*!
+ * jQuery Remote Clock 0.3.0
+ *
+ * Author: Andrii Volin <angy.volin@gmail.com>
+ * Released under the MIT license
+ * https://github.com/angyvolin/jquery-remote-clock/blob/master/LICENSE
+ */
 
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS style for Browserify
+        module.exports = factory;
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
     var old;
 
     var RemoteClock = function (element, options) {
@@ -136,4 +153,4 @@
 
         return this;
     };
-}(jQuery);
+}));
